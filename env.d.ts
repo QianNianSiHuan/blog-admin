1580ef1e258fd772a8bd4da33abeb161ae0bdc7a
aoge666 <1409050584@qq.com>
2024-12-26 10:07:24 +0800

    环境配置

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+export interface EnvMeta extends  Record<string, string>{
+    VITE_SERVER_URL: string
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,43 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import {loadEnv} from "vite";
+import {EnvMeta} from "./env";
 
+const envDir:string ="./"//env文件的目录
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  css: {
-    preprocessorOptions: {
-      less: {
-        modifyVars: {
-          //'primary-6': "red",
+export default defineConfig((config)=>{
+  const env =  loadEnv(config.mode,envDir) as  EnvMeta
+    return{
+        plugins: [
+            vue(),
+            vueDevTools(),
+        ],
+        css: {
+            preprocessorOptions: {
+                less: {
+                    modifyVars: {
+                        //'primary-6': "red",
+                    },
+                    additionalData: '@import "@/assets/var.less";',
+                    javascriptEnabled: true,
+                }
+            }
         },
-        additionalData: '@import "@/assets/var.less";',
-        javascriptEnabled: true,
-      }
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            },
+        },
+        envDir: envDir,
+        server: {
+            host: "0.0.0.0",
+            port: 80,
+            proxy:{
+                "/api":{
+                    target:env.VITE_SERVER_URL
+                }
+            }
+        }
     }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
 })
